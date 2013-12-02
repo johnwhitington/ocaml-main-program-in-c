23eee3a347f79c3fc6d74cf0db0300616ec3604b
John Whitington <john@coherentgraphics.co.uk>
2013-12-02 13:59:21 +0000

    more

--- prog.ml
+++ prog.ml
@@ -4,8 +4,8 @@ let provided_to_wrapper () =
 let _ = Callback.register "provided_to_wrapper" provided_to_wrapper
 
 let _ =
-  Array.iter ignore (Unix.environment ());
+  (*Array.iter ignore (Unix.environment ());
   let x = Bigarray.Array1.create Bigarray.int8_unsigned Bigarray.c_layout 1 in
-    x.{0} <- 1;
+    x.{0} <- 1;*)
     print_endline "Hello World, OCaml";;
 
