62dff3fd16f92dd8e5238aa50004e56d56f9a61f
John Whitington <john@coherentgraphics.co.uk>
2017-10-03 13:02:09 +0100

    Added camlparam0

--- wrapper.c
+++ wrapper.c
@@ -2,6 +2,7 @@
 #include <caml/memory.h>
 #include <caml/callback.h>
 int function_in_wrapper(void){
+   CAMLparam0();
    printf("Calling back into OCaml...\n");
    CAMLlocal2(provided_to_wrapper_v, from_callback);
    provided_to_wrapper_v = *caml_named_value("provided_to_wrapper");
